73d5b2d042a4ebc56c5df5779f71985b07c645a7
AnAtoliy_AK <aliaksandrau89@gmail.com>
2020-03-21 15:01:11 +0200

    feat: easy 5 test

--- src/index.js
+++ src/index.js
@@ -4,9 +4,28 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let arr = expr.split(' ').join('').split('');
+    let result = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '*') {
+            result += Number(arr[i - 1]) * Number(arr[i + 1]);
+        }
+        else if (arr[i] === '/') {
+            if (arr[i + 1] === '0') {
+                throw Error('TypeError: Division by zero.');
+            }
+            else result += Number(arr[i - 1]) / Number(arr[i + 1]);
+        }
+        else if (arr[i] === '-') {
+            result += Number(arr[i - 1]) - Number(arr[i + 1]);
+        }
+        else if (arr[i] === '+') {
+            result += Number(arr[i - 1]) + Number(arr[i + 1]);
+        }
+    }
+    return result;
+    //return SpeechRecognitionResult;
 }
-
 module.exports = {
     expressionCalculator
 }
