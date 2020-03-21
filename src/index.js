34aba03cebb5c26a73c8972eadae26c21616e902
AnAtoliy_AK <aliaksandrau89@gmail.com>
2020-03-21 19:57:55 +0200

    feat: add simple calc

--- src/index.js
+++ src/index.js
@@ -1,31 +1,90 @@
 function eval() {
-    // Do not use eval!!!
-    return;
+  // Do not use eval!!!
+  return;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function devide(a, b) {
+  if (b == 0) {
+    throw Error('TypeError: Division by zero.');
+  }
+  return a / b;
+}
+
+function sum(a, b) {
+  return parseFloat(a) + parseFloat(b);
+}
+function substract(a, b) {
+  return a - b;
+}
+
+function recMultDevideOper(ar) {
+  let result = [...ar];
+
+  while (result.findIndex(el => el === '*' || el === '/') !== -1) {
+    let elIndex = result.findIndex(el => el === '*' || el === '/');
+    let sign = result[elIndex];
+    if (sign === '/') {
+      result = removal(result, elIndex, devide);
+    }
+    if (sign === '*') {
+      result = removal(result, elIndex, multiply);
+    }
+  }
+  return result;
+}
+
+function recSumSubstractOper(ar) {
+  let result = [...ar];
+
+  while (result.findIndex(el => el === '+' || el === '-') !== -1) {
+    let elIndex = result.findIndex(el => el === '+' || el === '-');
+    let sign = result[elIndex];
+    if (sign === '+') {
+      result = removal(result, elIndex, sum);
+    }
+    if (sign === '-') {
+      result = removal(result, elIndex, substract);
+    }
+  }
+  return result;
+}
+
+function removal(remAr, elIndex, operationFunc) {
+  let res = [...remAr];
+  res.splice(elIndex - 1, 3, operationFunc(res[elIndex - 1], res[elIndex + 1]));
+  return res;
+}
+
+function transformDataStringToArray(expr) {
+  let arrayWithoutSpaces = expr.trim().split('').filter(e => e !== ' ');
+  let innerArray = [];
+  arrayWithoutSpaces.forEach((el, ind, arr) => {
+    if (!isNaN(+el) && !isNaN(arr[ind - 1])) {
+      let val = innerArray.pop() + el;
+      innerArray.push(val);
+    } else {
+      innerArray.push(el);
+    }
+  });
+  return innerArray;
 }
 
 function expressionCalculator(expr) {
-    let arr = expr.split(' ').join('').split('');
-    let result = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === '*') {
-            result += Number(arr[i - 1]) * Number(arr[i + 1]);
-        }
-        else if (arr[i] === '/') {
-            if (arr[i + 1] === '0') {
-                throw Error('TypeError: Division by zero.');
-            }
-            else result += Number(arr[i - 1]) / Number(arr[i + 1]);
-        }
-        else if (arr[i] === '-') {
-            result += Number(arr[i - 1]) - Number(arr[i + 1]);
-        }
-        else if (arr[i] === '+') {
-            result += Number(arr[i - 1]) + Number(arr[i + 1]);
-        }
-    }
-    return result;
-    //return SpeechRecognitionResult;
+  const dataArray = transformDataStringToArray(expr);
+  const multDevideResultArray = recMultDevideOper(dataArray);
+  const sumSubstrResultArray = recSumSubstractOper(multDevideResultArray);
+
+  return sumSubstrResultArray[0];
 }
 module.exports = {
-    expressionCalculator
+  expressionCalculator
 }
+
+
+
+
+
